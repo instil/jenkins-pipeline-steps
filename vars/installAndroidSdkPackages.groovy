d28c4c994344e2e484acd5266738ea40b36822d6
Chris van Es <cvanes@gmail.com>
2018-02-10 20:10:49 +0000

    Updating Android SDK installer and AVD launcher to use pipeline steps to execute tasks on build nodes rather than master

--- vars/installAndroidSdkPackages.groovy
+++ vars/installAndroidSdkPackages.groovy
@@ -1,19 +1,61 @@
-import co.instil.jenkins.AndroidSdkInstaller
-
-def call(List<String> packages = null) {
+def call(List<String> packages = []) {
     if (env.ANDROID_HOME == null) {
         failBuildWithError("ANDROID_HOME not defined, cannot install SDK packages")
     }
 
-    def sdkInstaller = new AndroidSdkInstaller(env.ANDROID_HOME)
-    if (packages != null) {
-        sdkInstaller.installSdkPackages(packages)
+    if (packages.size() <= 0) {
+        scanWorkspaceForPackagesInBuildScripts()
     } else {
-        sdkInstaller.scanRepositoryForBuildScripts()
+        installSdkPackages(packages)
     }
+}
+
+private def scanWorkspaceForPackagesInBuildScripts() {
+    // TODO: Add support for Xamarin
+    if (isGradleBuild()) {
+        installSdkPackagesForGradleBuild()
+    }
+}
+
+private def isGradleBuild() {
+    return sh(script: "find . -name build.gradle | wc -l", returnStdout: true).toInteger() > 0
+}
+
+private def installSdkPackagesForGradleBuild() {
+    def compileSdkVersions = sh(script: "find . -name build.gradle | xargs grep compileSdkVersion | grep -oE [0-9]+ || true", returnStdout: true)
+            .split()
+            .toList()
+            .collect { "platforms;android-$it" }
+    def buildToolsVersions = sh(script: "find . -name build.gradle | xargs grep buildToolsVersion | grep -oE [0-9]+.[0-9]+.[0-9]+ || true", returnStdout: true)
+            .split()
+            .toList()
+            .collect { "build-tools;$it" }
+
+    installSdkPackages(compileSdkVersions + buildToolsVersions)
+}
+
+private def installSdkPackages(List<String> requestedPackages) {
+    def requiredPackages = [
+            "platform-tools",
+            "tools",
+            "emulator",
+            "extras;android;m2repository",
+            "extras;google;m2repository",
+            "extras;intel;Hardware_Accelerated_Execution_Manager"
+    ]
+    def packages = (requiredPackages + requestedPackages).collect { "'$it'" }.join(" ")
+
+    echo("Installing Android SDK packages: $packages")
+    sh "$ANDROID_HOME/tools/bin/sdkmanager $packages"
+
+    echo("Accepting Android SDK licenses")
+    sh "yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses"
+
+    echo("Installing HAXM")
+    sh "$ANDROID_HOME/extras/intel/Hardware_Accelerated_Execution_Manager/silent_install.sh || true"
 }
 
 private def failBuildWithError(String message) {
     echo message
-    throw new IllegalArgumentException(message)
+    throw new IllegalStateException(message)
 }
--- vars/withAvd.groovy
+++ vars/withAvd.groovy
@@ -1,20 +1,72 @@
-import co.instil.jenkins.AvdLauncher
-
 def call(String hardwareProfile, String systemImage, Closure steps) {
     if (env.ANDROID_HOME == null) {
         failBuildWithError("ANDROID_HOME not defined, cannot launch AVD")
     }
 
-    def avdLauncher = new AvdLauncher(env.ANDROID_HOME)
-    avdLauncher.executeWithAvd(env.BUILD_TAG, hardwareProfile, systemImage) { emulatorSerial ->
+    executeWithAvd(hardwareProfile, systemImage, steps)
+}
+
+private def executeWithAvd(String hardwareProfile, String systemImage, Closure steps) {
+    def avdName = env.BUILD_TAG
+    def emulatorSerial = null
+    try {
+        installSystemImage(systemImage)
+        createAvd(avdName, hardwareProfile, systemImage)
+        emulatorSerial = launchAvd(avdName)
+
         // Set ANDROID_SERIAL env var so connectedAndroidTest knows which emulator
         // instance to target if we have multiple AVDs or concurrent jobs.
         env.ANDROID_SERIAL = emulatorSerial
-        steps()
+        steps(emulatorSerial)
+    } catch(any) {
+        throw any
+    } finally {
+        deleteAvd(avdName, emulatorSerial)
     }
+}
+
+private def installSystemImage(String systemImage) {
+    echo "Installing Android system image $systemImage"
+    sh "$ANDROID_HOME/tools/bin/sdkmanager '$systemImage'"
+    sh "yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses"
+}
+
+private def createAvd(String avdName, String hardwareProfile, String systemImage) {
+    echo "Creating AVD $avdName"
+    sh "$ANDROID_HOME/tools/bin/avdmanager create avd -n $avdName -f -k '$systemImage' -d '$hardwareProfile'"
+}
+
+private def launchAvd(String avdName) {
+    def emulatorPort = firstAvailablePortInRange(5554, 5682)
+    def emulatorSerial = "emulator-${emulatorPort}"
+
+    echo "Launching AVD $avdName with serial $emulatorSerial"
+
+    timeout(time: 5, unit: "MINUTES") {
+        sh "$ANDROID_HOME/emulator/emulator -avd $avdName -port $emulatorPort -no-window -no-boot-anim -no-audio -writable-system &"
+        sh "$ANDROID_HOME/platform-tools/adb -s $emulatorSerial wait-for-device"
+        waitUntil {
+            def bootCompleted = sh(script: "$ANDROID_HOME/platform-tools/adb -s ${emulatorSerial} shell getprop sys.boot_completed", returnStdout: true)
+            return bootCompleted.trim() == "1"
+        }
+    }
+
+    return emulatorSerial
+}
+
+private def firstAvailablePortInRange(start, end) {
+    return sh(
+            script: "(netstat -atn | awk '{printf \"%s\\n%s\\n\", \$4, \$4}' | grep -oE '[0-9]*\$'; seq ${start} ${end}) | sort -n | uniq -u | head -n 1",
+            returnStdout: true
+    ).trim()
+}
+
+private def deleteAvd(String avdName, String emulatorSerial) {
+    sh "$ANDROID_HOME/platform-tools/adb -s $emulatorSerial emu kill || true"
+    sh "$ANDROID_HOME/tools/bin/avdmanager delete avd -n $avdName || true"
 }
 
 private def failBuildWithError(String message) {
     echo message
-    throw new IllegalArgumentException(message)
+    throw new IllegalStateException(message)
 }
