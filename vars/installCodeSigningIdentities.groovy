9ce1b2fcf43b5df4baa16edde68f06c730ec82db
championofblocks <niallkelly808@gmail.com>
2019-09-27 15:05:18 +0100

    Specifying Fastlane keychain again

--- vars/installCodeSigningIdentities.groovy
+++ vars/installCodeSigningIdentities.groovy
@@ -5,6 +5,6 @@ def call(String appIdentifier, String type) {
         string(credentialsId: "FASTLANE_MATCH_GIT_URL", variable: "GIT_URL"),
         usernamePassword(credentialsId: 'APPLE_DEV_CENTRE_CREDENTIALS', passwordVariable: 'FASTLANE_PASSWORD', usernameVariable: 'FASTLANE_USER')
     ]) {
-        sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' git_url:'$GIT_URL' readonly:'true' verbose:'true'"
+        sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true'"
     }
 }
