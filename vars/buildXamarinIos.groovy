23dba6ec8fe12d7f7262a103b3b8b8632b7e6e36
Niall Kelly <niallkelly808@gmail.com>
2019-09-30 10:03:23 +0100

    adding Xamarin iOS build step and related description

---
+++ vars/buildXamarinIos.groovy
@@ -0,0 +1,35 @@
+def call(Map config) {
+
+    String solution = config.solution ?: findSolution()
+    if (solutionName == null) {
+    	failBuildWithError("No solution found. Please specify a solution explictly or ensure you are top level directory.")
+    }
+
+    String configuration = config.configuration ?: "Release"
+    String platform = config.platform ?: "iPhone"
+    boolean buildIpa = config.buildIpa ?: true
+    boolean archive = config.archive ?: true
+
+    build(solution, configuration, platform, clean, buildIpa, archive)
+}
+
+private def build(String solution, String configuration, String platform, boolean buildIpa, boolean archive) {
+	withCredentials([string(credentialsId: "FASTLANE_KEYCHAIN_PASSWORD", variable: "KEYCHAIN_PASSWORD")]) {
+        def keychain = "/Users/instil/Library/Keychains/fastlane.keychain-db"
+        sh "security unlock-keychain -p $keychainPassword ${keychain}"
+        sh "msbuild /p:Configuration=$configuration /p:Platform=$platform /p:BuildIpa=$buildIpa /p:ArchiveOnBuild=$archive /t:Clean /t:Build $solution /p:CodesignKeychain=${keychain}"
+        sh "security lock-keychain ${keychain}"
+    }
+}
+
+private def findSolution() {
+	return sh(script:"find . -type f -name '*.sln'")
+		.split()
+        .toList()
+        .first()
+}
+
+private def failBuildWithError(String message) {
+    echo message
+    throw new IllegalStateException(message)
+}
