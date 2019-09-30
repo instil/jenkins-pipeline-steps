75361faea378688974acc0c900d592c8964d9121
championofblocks <niallkelly808@gmail.com>
2019-09-30 12:56:15 +0100

    Minor style edits for Xamarin build script

--- vars/buildXamarinAppleOs.groovy
+++ vars/buildXamarinAppleOs.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
 
     String solution = config.solution ?: findSolution()
     if (solution == null) {
-    	failBuildWithError("No solution found. Please specify a solution explictly or ensure you are top level directory.")
+    	failBuildWithError("No solution found. Please specify a solution explictly or ensure you are in the top level directory.")
     }
 
     echo "Building ${solution} …"
@@ -18,9 +18,9 @@ def call(Map config) {
 private def build(String solution, String configuration, String platform, boolean buildIpa, boolean archive) {
 	withCredentials([string(credentialsId: "FASTLANE_KEYCHAIN_PASSWORD", variable: "KEYCHAIN_PASSWORD")]) {
         def keychain = "$HOME/Library/Keychains/fastlane.keychain-db"
-        sh "security unlock-keychain -p $keychainPassword ${keychain}"
-        sh "msbuild /p:Configuration=$configuration /p:Platform=$platform /p:BuildIpa=$buildIpa /p:ArchiveOnBuild=$archive /t:Clean /t:Build $solution /p:CodesignKeychain=${keychain}"
-        sh "security lock-keychain ${keychain}"
+        sh "security unlock-keychain -p $keychainPassword $keychain"
+        sh "msbuild /p:Configuration=$configuration /p:Platform=$platform /p:BuildIpa=$buildIpa /p:ArchiveOnBuild=$archive /t:Clean /t:Build $solution /p:CodesignKeychain=$keychain"
+        sh "security lock-keychain $keychain"
     }
 }
 
