147e1318fa5283185bfa377971cf592828fc7245
championofblocks <niallkelly808@gmail.com>
2019-12-05 16:05:14 +0000

    gitUrlCredentialIdentifier parameter with default value

--- vars/installCodeSigningIdentities.groovy
+++ vars/installCodeSigningIdentities.groovy
@@ -1,8 +1,8 @@
-def call(String appIdentifier, String type) {
+def call(String appIdentifier, String type, String gitUrlCredentialsId = "FASTLANE_MATCH_GIT_URL") {
     withCredentials([
         string(credentialsId: "FASTLANE_KEYCHAIN_PASSWORD", variable: "KEYCHAIN_PASSWORD"),
         string(credentialsId: "FASTLANE_MATCH_PASSWORD", variable: "MATCH_PASSWORD"),
-        string(credentialsId: "FASTLANE_MATCH_GIT_URL", variable: "GIT_URL"),
+        string(credentialsId: gitUrlCredentialsId, variable: "GIT_URL"),
         usernamePassword(credentialsId: 'APPLE_DEV_CENTRE_CREDENTIALS', passwordVariable: 'FASTLANE_PASSWORD', usernameVariable: 'FASTLANE_USER')
     ]) {
         sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true'"
