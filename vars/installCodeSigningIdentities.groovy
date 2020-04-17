006636d40ccbbd0f5e8ef744ed19ec54ffb5bf8d
championofblocks <niall.kelly@instil.co>
2020-04-17 11:24:10 +0100

    fixing param ref

--- vars/installCodeSigningIdentities.groovy
+++ vars/installCodeSigningIdentities.groovy
@@ -5,6 +5,6 @@ def call(String appIdentifier, String type, String matchPasswordCredentialsId = 
         string(credentialsId: gitUrlCredentialsId, variable: "GIT_URL"),
         usernamePassword(credentialsId: 'APPLE_DEV_CENTRE_CREDENTIALS', passwordVariable: 'FASTLANE_PASSWORD', usernameVariable: 'FASTLANE_USER')
     ]) {
-        sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true' generate_apple_certs:'${generate_apple_certs}'"
+        sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true' generate_apple_certs:'${generateAppleCerts}'"
     }
 }
