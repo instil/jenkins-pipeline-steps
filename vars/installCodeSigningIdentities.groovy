c68040c373bf38806cd778a08f08d3f41ce2844c
championofblocks <niall.kelly@instil.co>
2020-04-17 15:10:44 +0100

    reverting the generate apple certs changes
    Using fastlane defaults here instead

--- vars/installCodeSigningIdentities.groovy
+++ vars/installCodeSigningIdentities.groovy
@@ -1,10 +1,10 @@
-def call(String appIdentifier, String type, String matchPasswordCredentialsId = "FASTLANE_MATCH_PASSWORD", String gitUrlCredentialsId = "FASTLANE_MATCH_GIT_URL", boolean generateAppleCerts=false) {
+def call(String appIdentifier, String type, String matchPasswordCredentialsId = "FASTLANE_MATCH_PASSWORD", String gitUrlCredentialsId = "FASTLANE_MATCH_GIT_URL") {
     withCredentials([
         string(credentialsId: "FASTLANE_KEYCHAIN_PASSWORD", variable: "KEYCHAIN_PASSWORD"),
         string(credentialsId: matchPasswordCredentialsId, variable: "MATCH_PASSWORD"),
         string(credentialsId: gitUrlCredentialsId, variable: "GIT_URL"),
         usernamePassword(credentialsId: 'APPLE_DEV_CENTRE_CREDENTIALS', passwordVariable: 'FASTLANE_PASSWORD', usernameVariable: 'FASTLANE_USER')
     ]) {
-        sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true' generate_apple_certs:'${generateAppleCerts}'"
+        sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true'"
     }
 }
