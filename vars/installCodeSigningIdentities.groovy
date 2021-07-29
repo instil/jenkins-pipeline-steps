b5d4c945e5066ee9dd82e46981a0adba927c365f
championofblocks <niall.kelly@instil.co>
2021-07-29 14:37:44 +0100

    Adding devCenterCredentialsId with sensible default

--- vars/installCodeSigningIdentities.groovy
+++ vars/installCodeSigningIdentities.groovy
@@ -1,9 +1,9 @@
-def call(String appIdentifier, String type, String matchPasswordCredentialsId = "FASTLANE_MATCH_PASSWORD", String gitUrlCredentialsId = "FASTLANE_MATCH_GIT_URL") {
+def call(String appIdentifier, String type, String matchPasswordCredentialsId = "FASTLANE_MATCH_PASSWORD", String gitUrlCredentialsId = "FASTLANE_MATCH_GIT_URL", String devCenterCredentialsId = "APPLE_DEV_CENTRE_CREDENTIALS") {
     withCredentials([
         string(credentialsId: "FASTLANE_KEYCHAIN_PASSWORD", variable: "KEYCHAIN_PASSWORD"),
         string(credentialsId: matchPasswordCredentialsId, variable: "MATCH_PASSWORD"),
         string(credentialsId: gitUrlCredentialsId, variable: "GIT_URL"),
-        usernamePassword(credentialsId: 'APPLE_DEV_CENTRE_CREDENTIALS', passwordVariable: 'FASTLANE_PASSWORD', usernameVariable: 'FASTLANE_USER')
+        usernamePassword(credentialsId: devCenterCredentialsId, passwordVariable: 'FASTLANE_PASSWORD', usernameVariable: 'FASTLANE_USER')
     ]) {
         sh "fastlane run match app_identifier:'${appIdentifier}' type:'${type}' keychain_name:'fastlane.keychain' keychain_password:'$KEYCHAIN_PASSWORD' git_url:'$GIT_URL' readonly:'true' verbose:'true'"
     }
